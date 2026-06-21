01ca7b7ca1a77b8934fe5bf873f65623da78144b
Vxrpenter <vxrpenter@web.de>
2026-06-21 16:42:09 +0200

    Add direct registering of config objects

--- src/main/kotlin/dev/vxrp/configlite/ConfigLite.kt
+++ src/main/kotlin/dev/vxrp/configlite/ConfigLite.kt
@@ -1,9 +1,12 @@
 package dev.vxrp.configlite
 
 import com.charleskorn.kaml.Yaml
+import com.charleskorn.kaml.YamlConfiguration
+import dev.vxrp.configlite.exceptions.MalformedConfigurationObjectException
 import dev.vxrp.configlite.exceptions.RegisteredObjectNotFoundException
 import dev.vxrp.configlite.exceptions.ResourceNotFoundException
 import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import net.peanuuutz.tomlkt.Toml
 import net.peanuuutz.tomlkt.decodeFromString
@@ -52,6 +55,39 @@ open class ConfigLite {
     }
 
     /**
+     * Registering a configuration marks a configuration file as active.
+     * It then gets created and populated with a specified [config] value instead of a resource file,
+     * with a key being stored for access to the configuration file.
+     *
+     * @param headDirectory The head directory on the targeted system
+     * @param location Where should the configuration be created (excluded [headDirectory])
+     * @param name The filename of the configuration
+     * @param config The configuration to write
+     * @param type The type of the configuration (optional)
+     * @throws kotlinx.serialization.SerializationException
+     */
+    inline fun <reified T> registerDirect(headDirectory: String, location: String, name: String, config: T, type: ConfigType? = null) {
+        val configDirectory = Path("$headDirectory$location")
+
+        val configurationObject = ConfigurationObject(
+            location = configDirectory,
+            fileName = name,
+            type = type
+        )
+        registeredConfigurationObjects[name] = configurationObject
+
+        val file = Path("$configDirectory/$name").toFile()
+
+        if (configDirectory.notExists()) configDirectory.createDirectories()
+        if (!file.exists()) {
+            val content = encodeConfiguration<T>(configurationObject, config)
+
+            file.createNewFile()
+            file.appendText(content)
+        }
+    }
+
+    /**
      * Loads the configuration using the entered configuration class [T] by retrieving its file from the
      * [register]'ed configurations.
      *
@@ -64,10 +100,25 @@ open class ConfigLite {
     inline fun <reified T> load(name: String): T {
         val configuration = registeredConfigurationObjects[name] ?: throw RegisteredObjectNotFoundException("Could not find configuration file $name in registered configuration objects $registeredConfigurationObjects")
 
-        return serializedConfiguration<T>(configuration)
+        return decodeConfiguration<T>(configuration)
     }
 
-    inline fun <reified T> serializedConfiguration(configurationObject: ConfigurationObject): T {
+    inline fun <reified T> encodeConfiguration(configurationObject: ConfigurationObject, config: T): String {
+        if (configurationObject.type == null) {
+            if (configurationObject.fileName.endsWith(".json")) return encodeJson(config)
+            if (configurationObject.fileName.endsWith(".yml")) return encodeYaml(config)
+            if (configurationObject.fileName.endsWith(".toml")) return encodeToml(config)
+        }
+
+        return when(configurationObject.type) {
+            ConfigType.JSON -> encodeJson(config)
+            ConfigType.YAML -> encodeYaml(config)
+            ConfigType.TOML -> encodeToml(config)
+            null -> throw MalformedConfigurationObjectException("Configuration object $configurationObject filetype could not be determined for encoding")
+        }
+    }
+
+    inline fun <reified T> decodeConfiguration(configurationObject: ConfigurationObject): T {
         val file = Path("${configurationObject.location}/${configurationObject.fileName}").toFile()
         val fileContents = file.readText()
 
@@ -85,7 +136,15 @@ open class ConfigLite {
         }
     }
 
-    inline fun <reified T> decodeJson(fileContents: String) = Json.decodeFromString<T>(fileContents)
-    inline fun <reified T> decodeYaml(fileContents: String) = Yaml.default.decodeFromString<T>(fileContents)
-    inline fun <reified T> decodeToml(fileContents: String) =  Toml.decodeFromString<T>(fileContents)
+    val json = Json { prettyPrint = true }
+    val yaml = Yaml
+    val toml = Toml
+
+    inline fun <reified T> decodeJson(fileContents: String) = json.decodeFromString<T>(fileContents)
+    inline fun <reified T> decodeYaml(fileContents: String) = yaml.default.decodeFromString<T>(fileContents)
+    inline fun <reified T> decodeToml(fileContents: String) =  toml.decodeFromString<T>(fileContents)
+
+    inline fun <reified T> encodeJson(config: T) = json.encodeToString<T>(config)
+    inline fun <reified T> encodeYaml(config: T) = yaml.default.encodeToString<T>(config)
+    inline fun <reified T> encodeToml(config: T) =  toml.encodeToString<T>(config)
 }
